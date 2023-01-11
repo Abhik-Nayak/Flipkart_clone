2c0f0e8508f7ac9aa5122bf44948481c0238665d
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2023-01-11 20:45:10 +0530

    Abhik//11-01-23

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from './components/Layout';
-import {BrowswerRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './containers/Home';
 import Signin from './containers/Signin';
 import Signup from './containers/Signup';
@@ -8,14 +8,13 @@ import Signup from './containers/Signup';
 const App = () => {
   return (
     <div className='App'>
-      <Router>
-     
+      <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home}></Route>
           <Route exact path="/Signup" component={Signup}></Route>
           <Route exact path="/Signin" component={Signin}></Route>
         </Switch>
-      </Router>
+      </BrowserRouter>
     </div>
   )
 }
--- front_end/src/containers/Signin/index.js
+++ front_end/src/containers/Signin/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Layout from '../../components/Layout';
+import {Container , Form, Row, Col, Button} from "react-bootstrap";
 
 
 const Signin = () => {
   return (
-    <div>signin</div>
+    <Layout>
+      <Container>
+        <Row>
+          <Col>
+          </Col>
+        </Row>
+        <Form>
+          <Form.Group controlId='formBasicEmail'>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type='email' placeholder="enter your email address"/>
+            <Form.Text className='text-muted'> We will never share your email with others.</Form.Text>
+          </Form.Group>
+        </Form>
+      </Container>
+    </Layout>
   )
 }
 
