ecc2fdb7fe4c0747e3a165610da6935ab91f806c
Abhik Nayak <72656033+Abhik-Nayak@users.noreply.github.com>
2023-01-15 06:27:26 +0530

    Abhik//15-01-2023

--- back_end/src/common-middleware/index.js
+++ back_end/src/common-middleware/index.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken");
+
 exports.requireSignin = (req, res, next) => {
     // const token = req.headers.authorization;
     // console.log(token);
@@ -6,17 +8,24 @@ exports.requireSignin = (req, res, next) => {
         console.log(token);
         const user = jwt.verify(token, process.env.JWT_SECRET);
         req.user = user;
-        next();
     }
-    return res.status(400).json({message: 'Authorization required'})
+    else {
+        return res.status(400).json({ message: 'Authorization required' })
+    }
+    next();
 }
 
 exports.userMiddleware = (req, res, next) => {
+    if (req.user.role !== 'user') {
+        return res.status(400).json({ message: "User Access denied" })
+    }
+    next();
 
 }
 
 exports.adminMiddleware = (req, res, next) => {
-    if (req.user.role != 'admin') {
+    console.log(req.user.role);
+    if (req.user.role !== 'admin') {
         return res.status(400).json({ message: "Access denied" })
     }
     next();
---
+++ back_end_pg4/src/index.js
@@ -0,0 +1,110 @@
+// const express = require("express");
+// const { Client } = require('pg');
+
+// const client = new Client({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: 'admin',
+//   port: 5432,
+// })
+// client.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+// });
+// console.log("index");
+
+// const cors = require('cors');
+// const express  = require('express');
+// const bodyParser = require('body-parser');
+// var typeorm = require("typeorm"); var EntitySchema = typeorm.EntitySchema;
+// const http = require('http');
+// // const app  = require('./app');
+// const app = express();
+
+// app.use(cors({
+//     origin: "*",
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }))
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+// app.use(express.json())
+
+// var connection =  typeorm.createConnection({
+//     "type": "postgres",
+//     "host": "localhost",
+//     "port": 5432,
+//     "username": "postgres",
+//     "password": "admin",
+//     "database": "postgres",
+//     "synchronize": false,
+//     "logging": false,
+//     // entities: [
+//     //     new EntitySchema(require("./entities/posts.json"))
+//     // ]
+// }).then(function (connection) {
+//     console.log("connected");
+//     const server =http.createServer(app);
+//     server.listen(5000);
+
+// }).catch(function (error) {
+//     console.log("Error: ", error)
+//     return;
+// });
+
+// module.exports = connection
+
+const express = require("express");
+const env = require("dotenv");
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+var typeorm = require("typeorm");
+var EntitySchema = typeorm.EntitySchema;
+const http = require('http');
+
+// environment  variable or you can say constants
+env.config();
+
+app.use(cors({
+    origin: "*",
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.use(express.json())
+
+var connection = typeorm.createConnection({
+    "type": "mongodb",
+    "host": "localhost",
+    "port": 27017,
+    "database": "flipkart_clone_db",
+    "synchronize": false,
+    "logging": false,
+    // "type": "postgres",
+    // "host": "localhost",
+    // "port": 5432,
+    // "username": "postgres",
+    // "password": "admin",
+    // "database": "postgres",
+    // "synchronize": false,
+    // "logging": false,
+    // entities: [
+    //     new EntitySchema(require("./entities/posts.json"))
+    // ]
+}).then(function (connection) {
+    console.log("connected");
+    const server = http.createServer(app);
+    server.listen(5000);
+
+}).catch(function (error) {
+    console.log("Error: ", error)
+    return;
+});
+
+module.exports = connection
+
